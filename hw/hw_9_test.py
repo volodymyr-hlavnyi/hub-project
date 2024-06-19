903eb3525d7e0cae17b61e222e29ccac83b5014c
volodymyr-hlavnyi <volodymyr.hlavnyi@gmail.com>
2024-06-19 16:44:39 +0200

    hw 9 test

--- hw/hw_9_test.py
+++ hw/hw_9_test.py
@@ -12,20 +12,22 @@ def random_string(length):
 def test_speed(length_string: int = 100000):
     start = time.time()
     for i in range(length_string):
-        a = random_string(10) + random_string(10) + random_string(10)
+        a = random_string(10) + ' ' + random_string(10) + ' ' + random_string(10)
     time_plus = time.time() - start
     print('Time for operator +:', time_plus)
 
     start = time.time()
     for i in range(length_string):
-        a = f'{random_string(10)}{random_string(10)}{random_string(10)}'
+        a = f'{random_string(10)} {random_string(10)} {random_string(10)}'
     time_fstring = time.time() - start
     print('Time for operator f-string:', time_fstring)
     print(' -> ', (time_plus - time_fstring))
 
 
 if __name__ == '__main__':
+    test_number = 1
     for i in range(0, 30, 10):
         real_length = i * 100000
-        print(f'test {i}: For string length of {real_length}')
+        print(f'test {test_number}: For string length of {real_length}')
         test_speed(real_length)
+        test_number += 1
