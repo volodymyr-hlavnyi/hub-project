4ad6e692007636a156d7c8fc463680794f1b9649
volodymyr-hlavnyi <volodymyr.hlavnyi@gmail.com>
2024-07-06 00:10:00 +0200

    hw 20

--- hw/hw_20.py
+++ hw/hw_20.py
@@ -67,6 +67,50 @@ def count_unique_chars(string_for_check):
     return len(set(string_for_check))
 
 
+# 3. Напишите программу, которая создает словарь, содержащий информацию
+# о студентах и их оценках. Ключами словаря являются имена студентов,
+# а значениями - списки оценок. Создайте функцию calculate_average_grade,
+# которая принимает словарь с оценками студентов и
+# вычисляет средний балл для каждого студента.
+# Функция должна возвращать новый словарь, в котором ключами являются
+# имена студентов, а значениями - их средний балл.
+# Выведите результат на экран.
+#
+#
+# Пример словаря с оценками
+#
+# grades = {
+#     'Alice': [85, 90, 92],
+#     'Bob': [78, 80, 84],
+#     'Carol': [92, 88, 95]
+# }
+#
+# Пример вывода:
+#
+# {'Alice': 89.0, 'Bob': 80.66666666666667, 'Carol': 91.66666666666667}
+
+def input_grades():
+    grades = {}
+    while True:
+        student_name = input('Enter student name (empty string - exit): ')
+        if len(student_name) == 0:
+            break
+        grades[student_name] = []
+        while True:
+            grade = input('Enter grade (empty string - exit): ')
+            if len(grade) == 0:
+                break
+            grades[student_name].append(int(grade))
+    return grades
+
+
+def calculate_average_grade(grades):
+    result = {}
+    for student, grades_list in grades.items():
+        result[student] = sum(grades_list) / len(grades_list)
+    return result
+
+
 if __name__ == '__main__':
     print('1. Merge dicts')
     list_dicts = merge_digit_input()
@@ -78,3 +122,15 @@ if __name__ == '__main__':
     print('2. Count unique chars')
     input_string = input('Enter string: ')
     print(f'Count of unique chars: {count_unique_chars(input_string)}')
+
+    print('3. Calculate average grade')
+    grades = input_grades()
+    if len(grades) == 0:
+        grades = {
+            'Alice': [85, 90, 92],
+            'Bob': [78, 80, 84],
+            'Carol': [92, 88, 95]
+        }
+    print(grades)
+    print(calculate_average_grade(grades))
+
--- unittest/hw_20_unittest.py
+++ unittest/hw_20_unittest.py
@@ -1,5 +1,5 @@
 import unittest
-from hw.hw_20 import merge_digits, count_unique_chars
+from hw.hw_20 import merge_digits, count_unique_chars, calculate_average_grade
 
 
 class TestHomeworkFunction(unittest.TestCase):
@@ -11,6 +11,21 @@ class TestHomeworkFunction(unittest.TestCase):
     def test_hw20_2(self):
         self.assertEqual(count_unique_chars('Hello, World!'), 10)
         self.assertEqual(count_unique_chars('hello'), 4)
+
+    def test_hw20_3(self):
+        a = {
+            'Alice': [85, 90, 92],
+            'Bob': [78, 80, 84],
+            'Carol': [92, 88, 95]
+        }
+        a_result = {'Alice': 89.0, 'Bob': 80.66666666666667, 'Carol': 91.66666666666667}
+        self.assertEqual(calculate_average_grade(a), a_result)
+        b = {
+            'Ivanov': [4, 5, 3, 4],
+            'Petrov': [3, 4, 5, 5]
+        }
+        b_result = {'Ivanov': 4.0, 'Petrov': 4.25}
+        self.assertEqual(calculate_average_grade(a), a_result)
 
 
 if __name__ == '__main__':
