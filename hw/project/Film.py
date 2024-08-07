4a9dc2a1772680e0ff831fab42319f6288aa9d82
volodymyr-hlavnyi <volodymyr.hlavnyi@gmail.com>
2024-08-07 09:06:34 +0200

    hw_38 final task remove to other Project

--- hw/project/Film.py
+++
@@ -1,9 +0,0 @@
-class Film:
-    def __init__(self, data):
-        self.id = data[0]
-        self.title = data[1]
-        self.release_year = data[2]
-        self.description = data[3]
-
-    def __str__(self):
-        return f"{self.title} {self.release_year}"
